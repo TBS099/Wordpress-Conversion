7d9deafb127302d2ba7b39bbdb56d8c6226d5fe2
TBS099 <tanushbikram72@gmail.com>
2023-02-28 17:39:27 +0545

    integrating WooCommerce into theme

--- functions.php
+++ functions.php
@@ -35,10 +35,10 @@ function load_stylesheets()
 }
 
 //Adding WooCommerce Support
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-   add_theme_support( 'woocommerce' );
-}  
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 //Registering the Menus
 register_nav_menus(
